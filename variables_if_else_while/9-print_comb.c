262452e595f36b49b596db542b2a0023c1a80d64
pablosalina25 <you@example.com>
2023-10-11 10:08:32 -0700

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,19 +5,19 @@
  *
  * Return: Always 0 (Success)
  */
-	int main(void)
+int main(void)
 {
 	int numero;
 
-	for (numero = '0'; numero <= '9'; numero++)
-	{
-	putchar(numero);
-	if (numero != '9')
-	{
-		putchar(',');
-		putchar(' ');
-	}
+	for (numero = 0; numero <= 9; numero++)
+{
+	putchar(numero + '0');
+	if (numero != 9)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
 	putchar('\n');
-	}
 	return (0);
 }
