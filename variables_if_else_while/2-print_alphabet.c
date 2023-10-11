653466ce592c873d8826f79df490bf32b7450a73
pablosalina25 <you@example.com>
2023-10-11 07:12:55 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+	int main(void)
+	{
+		for (char letra 'a'; letra <= 'z' ; letra++)
+		putchar(letra);
+
+	}
+	putchar('\n');
+	return (0);
+	}
