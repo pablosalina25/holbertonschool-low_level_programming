0fbd4059f089aa63340eab9663d01eb8ba5f87cd
pablosalina25 <you@example.com>
2023-10-11 07:22:10 -0700

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -5,14 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
-	int main(void
+	int main(void)
+{
 	char letra;
-	{
+	
 		for (letra = 'a'; letra <= 'z' ; letra++)
-		{
+		
 		putchar(letra);
-
-		}
+}
 	putchar('\n');
 	return (0);
 	}
