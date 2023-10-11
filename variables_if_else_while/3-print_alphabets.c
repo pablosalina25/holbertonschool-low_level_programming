a35505582a28ad27d22b67cce0083a8d0dad8ca2
pablosalina25 <you@example.com>
2023-10-11 07:34:01 -0700

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letra_mayuscula;
+
+	for (letra = 'A'; letra <= 'Z'; letra++)
+		putchar(letra);
+
+	putchar('\n');
+	return (0);
+}
