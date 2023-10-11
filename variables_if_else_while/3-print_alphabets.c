35a109f9e97bd4144637c7c66cbabb62f9b5608d
pablosalina25 <you@example.com>
2023-10-11 07:47:29 -0700

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -12,7 +12,7 @@ int main(void)
 	for (letra = 'a'; letra <= 'z'; letra++)
 		putchar(letra);
 	for (letra = 'A'; letra <= 'Z'; letra++)
-		putchar(letra)
+		putchar(letra);
 	putchar('\n');
 	return (0);
 }
