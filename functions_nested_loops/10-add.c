91950302654b7efc3769cddc2f091a7bba90565a
pablosalina25 <you@example.com>
2023-10-13 10:08:07 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * int add(int, int); Write a function that adds two integers
+ * Return num3
+ */
+int add(int num1, int num2)
+{
+	int num3;
+
+	num3 = num1 + num2;
+
+	return (num3);
+}
