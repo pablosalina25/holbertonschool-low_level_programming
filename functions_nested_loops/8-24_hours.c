2bdf3703d35442f3e0c30abafc8e07b0b415588f
pablosalina25 <you@example.com>
2023-10-14 14:00:50 -0700

    e

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - A function that prints every minute of the day
- * @n starting from 00:00 to 23:59.
+ * jack_bauer - Prints every minute of the day starting from 00:00 to 23:59.
  */
 void jack_bauer(void)
 {
