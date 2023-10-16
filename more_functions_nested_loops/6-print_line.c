ac19dd9d7af4824e6e6fac3bcd6808d3c56f672f
pablosalina25 <you@example.com>
2023-10-16 07:58:32 -0700

    6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -8,15 +8,14 @@
  */
 void print_line(int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n; i++)
-    {
-        _putchar('_');
-    }
-
-    if (n >= 0)
-    {
-        _putchar('\n');
-    }
+	for (i = 0; i < n; i++)
+	{
+	_putchar('_');
+	}
+	if (n >= 0)
+	{
+	_putchar('\n');
+	}
 }
