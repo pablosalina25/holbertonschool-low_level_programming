e56fe397b6b5bd664fc272c82dfe0253ab7eb96f
pablosalina25 <you@example.com>
2023-10-16 09:18:36 -0700

    6-print_line.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,7 +2,7 @@
 
 /**
  * print_diagonal - a function that draws a diagonal line on the terminal.
- * @n: the number of times the character / should be printed
+ * @n: the number of times the character \ should be printed
  *
  * Return: If n is 0 or less, the function should only print \n
  */
@@ -20,6 +20,7 @@ void print_diagonal(int n)
 	{
 	_putchar(' ');
 	}
-	_putchar('/');
+	_putchar('\');
 	_putchar('\n');
-	}}
+	}
+}
