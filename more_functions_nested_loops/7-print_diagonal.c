643d0baeaee3a14ad10874f475c1b667fae01347
pablosalina25 <you@example.com>
2023-10-16 10:34:44 -0700

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,11 +12,13 @@ void print_diagonal(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(92);
 	for (j = 0; j < n-i; j++)
+		
 			{
 			_putchar(' ');
+
 			}
+	_putchar(92);
 	_putchar('\n');
 			}
 }
