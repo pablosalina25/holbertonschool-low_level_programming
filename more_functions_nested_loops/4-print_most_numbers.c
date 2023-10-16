940517cdb54357c6911289ae128e53c4f67adda5
pablosalina25 <you@example.com>
2023-10-16 06:27:54 -0700

    j

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_numbers - prints the numbers from 0 to 9
+ * print_most_numbers - prints the numbers from 0 to 9
  * Do not print 2 and 4
  */
 void print_most_numbers(void)
