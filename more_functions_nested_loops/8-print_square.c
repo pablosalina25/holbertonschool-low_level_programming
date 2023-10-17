669a22155dbbed8a7f119aaea8e69898f936f8e1
pablosalina25 <you@example.com>
2023-10-17 06:42:00 -0700

    8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -9,17 +9,18 @@ void print_square(int size)
 {
 	if (size <= 0)
 	{
-	_putchar('\n');
-	}
-	else
+		_putchar('\n');
+	} else
 	{
-	for (int i = 0; i < size; i++)
-	{
-	for (int j = 0; j < size; j++)
-	{
-	_putchar('#');
-	}
-	_putchar('\n');
-	}
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
