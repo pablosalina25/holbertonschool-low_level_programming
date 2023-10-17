a37b766ce9dfdf5bbe0aa83b402a7c8cb96a8efd
pablosalina25 <you@example.com>
2023-10-17 07:36:03 -0700

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,22 +15,24 @@ int main(void)
 	{
 	if (n % 3 == 0 && n % 5 == 0)
 	{
-	printf("FizzBuzz ");
+	printf("FizzBuzz");
 	}
 	else if (n % 3 == 0)
 	{
-	printf("Fizz ");
+	printf("Fizz");
 	}
 	else if (n % 5 == 0)
 	{
-	printf("Buzz ");
+	printf("Buzz");
 	}
-	else
+	else if
 	{
 	printf("%d ", n);
 	}
 	}
+	printf (" ");
 	printf("\n");
+	
 
 	return (0);
 }
