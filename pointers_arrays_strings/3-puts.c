af67493e98840d7ff499c95c89f2b2191fbc15b2
pablosalina25 <you@example.com>
2023-10-18 09:41:01 -0700

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * void _puts - a function that prints a string.
+ * @str: The input string
+ * Return: the string
+ */
+void _puts(char *str)
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+	length++;
+	puts(str);
+	}
+}
