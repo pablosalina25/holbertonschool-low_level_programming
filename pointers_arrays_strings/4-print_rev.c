6eede1e0a51dd50acece3c5ccb07e2eac8333538
pablosalina25 <you@example.com>
2023-10-20 05:22:07 -0700

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * void print_rev - prints a string, in reverse,
+ * @s: The input string to be printed.
+ *
+ * Description: This function prints the string
+ * in reverse
+ */
+kiiiiiiivoid print_rev(char *s)
+{
+	int i,mitad,len;
+	len = strlen(s)
+		mita = len/2;
+
+	for (i=0;i<mitad;i++
+}
