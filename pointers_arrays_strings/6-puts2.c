9014c4df603edbe03cee3946d96b12bdbc8254d0
pablosalina25 <you@example.com>
2023-10-20 10:26:21 -0700

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string.
+ * @str: The input string to be printed.
+ *
+ * Description: This function prints the given
+ * string to the standard output.
+ */
+void _puts(char *str)
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+	length++;
+		if (str == % 0)
+	}
+	puts(str);
+}
