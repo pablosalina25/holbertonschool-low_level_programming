d5bdd623dbdac8bb37ff5b28f54986af64f8dd46
pablosalina25 <you@example.com>
2023-10-25 10:39:32 -0700

    5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,12 +1,28 @@
 #include "main.h"
 #include <stddef.h>
 
-/** _strstr - locates a substring.
- * Return: a pointer to the beginning of the located
- *  substring, or NULL.
+/**
+ * _strstr - a function that locates a substring.
+ * @str: The string to be capitalized.
+ *
+ * Return:a pointer to the beginning of the located substring
  */
+
 char *_strstr(char *haystack, char *needle)
 {
 	while (*haystack != '\0')
-		haystack++;
+	{
+	char *lim = haystack;
+	char *pim = needle;
+
+	while (*lim == *pim && *pim != '\0')
+	{
+	lim++;
+	pim++;
+	}
+	if (*p == '\0')
+	return (haystack);
+	haystack++;
+	}
+	return (NULL);
 }
