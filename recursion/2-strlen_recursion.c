1e9e6b18399a98b95a8b42a0a601bd427d48b791
pablosalina25 <you@example.com>
2023-10-26 06:54:15 -0700

    2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -8,11 +8,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	int length = 0;
-
 	if (*s == '\0')
 	{
-	length++;
+	return (0);
 	}
-	return (length);
+	else
+	{
+	return (1 + _strlen_recursion(s + 1));
+	}
 }
