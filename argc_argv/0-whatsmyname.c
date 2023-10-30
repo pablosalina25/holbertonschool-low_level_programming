89a23f189b3c4c61d3d53d0f131fda05a70c653e
pablosalina25 <you@example.com>
2023-10-30 06:14:48 -0700

    primero

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: Always 0 (Success)
- * /
+ */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
