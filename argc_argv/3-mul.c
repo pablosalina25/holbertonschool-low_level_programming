8b3158fe91519ee7e95dc2c33195c5ea3f62011e
pablosalina25 <you@example.com>
2023-10-30 07:31:19 -0700

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -2,23 +2,25 @@
 #include <stdlib.h>
 
 /**
- * main - a program that multiplies two numbers.
- * atoi - converts a string to an integer
- * @s: string to be converted
- * Return: the int converted from the string|
- **/
+ * main - Multiplies two numbers.
+ *
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
 
 int main(int argc, char *argv[])
 {
+	int numero1 = atoi(argv[1]);
+	int numero2 = atoi(argv[2]);
+	int resultado = numero1 * numero2;
+
 	if (argc != 3)
 	{
 	printf("Error\n");
 	return (1);
 	}
-	int numero1 = atoi(argv[1]);
-	int numero2 = atoi(argv[2]);
-	int resultado = numero1 * numero2;
-
 	printf("%d\n", resultado);
 	return (0);
 }
