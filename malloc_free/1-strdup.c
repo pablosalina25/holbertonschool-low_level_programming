dfc8f97d393bcda0ba63d4d8a63e6a05e7e98c5c
pablosalina25 <you@example.com>
2023-11-03 09:26:27 -0700

    cuarta vez que lo arreglo

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
- *_strdup - Duplicates a string into a newly
- * allocated memory space.
+ * _strdup - Duplicates a string into
+ *  a newly allocated memory space.
  * @str: The input string to duplicate.
  * Return: A pointer to the duplicated string,
- * or NULL if allocation fails.
+ *  or NULL if allocation fails.
  */
 char *_strdup(char *str)
 {
-	int longi = 0;
+	char *copy;
+	int longit = 0;
+	int i;
 
 	if (str == NULL)
 	return (NULL);
-
-	while (str[longi] != '\0')
-	longi++;
-
-	char *copy = malloc(sizeof(char) * (longi + 1));
-
+	while (str[longit] != '\0')
+	longit++;
+	copy = (char *)malloc(sizeof(char) * (longit + 1))
 	if (copy == NULL)
 	return (NULL);
-
-	for (int i = 0; i <= longi; i++)
+	for (i = 0; i <= longit; i++)
 	copy[i] = str[i];
 	return (copy);
 }
