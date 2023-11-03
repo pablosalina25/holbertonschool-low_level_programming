eb75080cdffd2598c681406b84c1ab1e77eb2d23
pablosalina25 <you@example.com>
2023-11-03 11:34:44 -0700

    vz 20

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,32 +9,33 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int len1 = 0, len2 = 0;
+	int len1 = 0, len2 = 0, i;
+	char *concatenated;
 
 	if (s1 == NULL)
 	s1 = "";
 	if (s2 == NULL)
 	s2 = "";
+
 	while (s1[len1])
 	len1++;
 	while (s2[len2])
 	len2++;
 
-	char *concatenated = malloc(len1 + len2 + 1);
-
+	concatenated = malloc(len1 + len2 + 1);
 	if (concatenated == NULL)
-
 	return (NULL);
 
-	for (int i = 0; i < len1; i++)
+	for (i = 0; i < len1; i++)
 	{
 	concatenated[i] = s1[i];
 	}
-	for (int i = 0; i < len2; i++)
+	for (int j = 0; j < len2; j++)
 	{
-	concatenated[len1 + i] = s2[i];
+	concatenated[i] = s2[j];
+	i++;
 	}
-	concatenated[len1 + len2] = '\0';
+	concatenated[i] = '\0';
 
 	return (concatenated);
 }
