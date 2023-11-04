ec76a05c6b505197135ef0a18d17f1ca458ad719
pablosalina25 <you@example.com>
2023-11-04 12:43:07 -0700

    cuarta

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees a 2-dimensional grid previously
+ * created by alloc_grid.
+ * @grid: The 2D grid to be freed.
+ * @height: The height dimension of the grid.
+ *
+ * Description: This function frees the memory allocated
+ *  for a 2D grid
+ * previously created by the alloc_grid
+ * function.
+ */
+void free_grid(int **grid, int height)
+{
+	int Z;
+
+	if (grid != NULL)
+	{
+		for (Z = 0; height > Z; Z++)
+		{
+			free(grid[Z]);
+		}
+		free(grid);
+	}
+}
