302c76d6905831bc3e0b125e1d4bf91b3ac70739
pablosalina25 <you@example.com>
2023-11-05 11:16:18 -0800

    2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This is the main function of the program.
+ * It prints the name of the current source
+ * file to the standard output.
+ * Return: Always returns 0 to
+ * indicate successful execution.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
