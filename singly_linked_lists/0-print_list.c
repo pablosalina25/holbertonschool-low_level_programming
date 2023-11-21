8a6b3b2b72e71f417f993917cc23d6e8333b800a
pablosalina25 <you@example.com>
2023-11-21 10:06:08 -0800

    a

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 #include <stdio.h>
+
 /**
- * print_list - a function that prints all the elements of a list_t.
- * @h:h
- * Return: numero de nodos
+ * print_list - A function that prints all the elements of a list_t.
+ * @h: h
+ * Return: Number of nodes.
  */
 size_t print_list(const list_t *h)
 {
@@ -14,8 +15,9 @@ size_t print_list(const list_t *h)
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
 		numero_nodos++;
 	}
 	return (numero_nodos);
