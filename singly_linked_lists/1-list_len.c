4ce739505208a3700d89b60e4f1cef99b7b26281
pablosalina25 <you@example.com>
2023-11-22 05:24:39 -0800

    1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * list_len - a function that returns the number
-of elements in a linked list_t list.
+ * list_len - a function that
+ * returns the number of elements in a linked list_t list.
  * @h: h
  * Return: elementos_list
  */
