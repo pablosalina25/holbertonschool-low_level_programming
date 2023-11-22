9ec98dd39b3fc18876a49c5bf91e4609241f1326
pablosalina25 <you@example.com>
2023-11-22 04:53:50 -0800

    h

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - a function that returns the number
+of elements in a linked list_t list.
+ * @h: h
+ * Return: the number of elements in a linked list_t
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int elementos_list = 0;
+
+	while (h)
+	{
+		if (!h->len)
+		printf("[0] (nill)\n");
+		else
+		printf("[%u] %i\n", h->len, h->len);
+		h = h->next;
+		elementos_list++;
+}
+		return (elementos_list);
+}
