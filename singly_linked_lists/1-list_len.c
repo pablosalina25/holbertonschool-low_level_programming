0eda8469e9444ec8e13d527ddcb09e4d437e4e61
pablosalina25 <you@example.com>
2023-11-22 05:19:08 -0800

    h

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -5,7 +5,7 @@
  * list_len - a function that returns the number
 of elements in a linked list_t list.
  * @h: h
- * Return: the number of elements in a linked list_t
+ * Return: elementos_list
  */
 size_t list_len(const list_t *h)
 {
