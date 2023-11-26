cf64cb02ea1f06c8e8e1cfeef470eb7988273387
pablosalina25 <you@example.com>
2023-11-26 14:04:34 -0800

    e

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,35 +1,43 @@
 #include "lists.h"
+
 /**
  * add_node_end - Adds a new node at the end of a list_t list.
- * @head: The first node
+ * @head: The first node.
  * @str: The string.
  * Return: Address of the new element or NULL if it fails.
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node_end(head, str)
+    list_t **head;
+    const char *str;
 {
-	if (!str)
-	return (NULL);
-	list_t *endnode = malloc(sizeof(list_t));
-	if (!endnode)
+    if (!str)
+        return (NULL);
 
-	{
-	free(endnode);
-	return (NULL);
-	}
-	endnode->str = strdup(str);
-	endnode->len = strlen(str);
-	endnode->next = NULL;
+    list_t *endnode;
+    endnode = malloc(sizeof(list_t));
+    if (!endnode)
+    {
+        free(endnode);
+        return (NULL);
+    }
 
-	if (*head == NULL)
-	{
-	*head = endnode;
-	return (endnode);
-	}
-	list_t *tmp;
+    endnode->str = strdup(str);
+    endnode->len = strlen(str);
+    endnode->next = NULL;
 
-	for (tmp = *head; tmp->next; tmp = tmp->next)
-	{
-	}
-	tmp->next = endnode;
-	return (endnode);
+    if (*head == NULL)
+    {
+        *head = endnode;
+        return (endnode);
+    }
+
+    list_t *tmp;
+    for (tmp = *head; tmp->next; tmp = tmp->next)
+    {
+    }
+
+    tmp->next = endnode;
+
+    return (endnode);
 }
+
