c67377f064fb1ade5830b848e6ee426da78595b6
pablosalina25 <you@example.com>
2023-12-02 12:02:56 -0800

    d

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -9,12 +9,13 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int bitmask;
+
 	if (index >= (sizeof(unsigned long int) * 8))
 	return (-1);
 
-	unsigned long int bitmask = 1UL << index;
+	bitmask = 1UL << index;
 	*n &= ~bitmask;
 
 	return (1);
 }
-
