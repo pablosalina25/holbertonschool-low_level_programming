f508452264a0d4f1b4bdf6387ae8e0651986a992
pablosalina25 <pablosalina25@gmail.com>
2024-01-18 11:16:43 -0800

    task 1 fixed array

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -9,24 +9,26 @@
  * Return: If the value is not present or the array is NULL, -1.
  * Otherwise, the index where the value is located.
  */
-int binary_search(int *array, size_t array_size, int target_value)
+int binary_search(int *array, size_t size, int value)
 {
-	size_t low_index, high_index, mid_index;
+	size_t low_index = 0, high_index = size - 1, mid_index;
 
-	if (array == NULL || array_size == 0)
+	if (array == NULL || size == 0)
 	return (-1);
-
-	low_index = 0;
-	high_index = array_size - 1;
 
 	while (low_index <= high_index)
 	{
+	printf("Searching in array: ");
+	for (size_t i = low_index; i < high_index; i++)
+	printf("%d, ", array[i]);
+	printf("%d\n", array[high_index]);
+
 	mid_index = low_index + (high_index - low_index) / 2;
 
-	if (array[mid_index] == target_value)
+	if (array[mid_index] == value)
 	return (mid_index);
 
-	if (array[mid_index] < target_value)
+	if (array[mid_index] < value)
 	low_index = mid_index + 1;
 	else
 	high_index = mid_index - 1;
